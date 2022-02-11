3c06d3fb3b4ae261c445a3ee49b8e3d82e2a02eb
Nikola Svetozarevic <nikola.svetozarevic.e@gmail.com>
2022-02-11 16:45:54 +0100

    Add Client entity & refactor migrations

--- database/migrations/2022_02_11_135526_create_clients_table.php
+++ database/migrations/2022_02_11_135526_create_clients_table.php
@@ -7,19 +7,16 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('clients', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->text('description');
             $table->timestamps();
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('clients');
     }
 };
