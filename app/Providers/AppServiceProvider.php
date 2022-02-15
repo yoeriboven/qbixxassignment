586adeafb633a5f66fcb3f6179191dbf9fa3c103
Nikola Svetozarevic <nikola.svetozarevic.e@gmail.com>
2022-02-15 11:47:51 +0100

    Config factories

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Factory::guessFactoryNamesUsing(static function (string $modelName): string {
+            return 'Database\\Factories\\'.class_basename($modelName).'Factory';
+        });
     }
 }
