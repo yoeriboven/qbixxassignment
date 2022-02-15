04f223541cedaa1891a3eff49f5346da507af467
Nikola Svetozarevic <nikola.svetozarevic.e@gmail.com>
2022-02-15 21:39:03 +0100

    Add title on welcome page

--- resources/views/pages/front/welcome.blade.php
+++ resources/views/pages/front/welcome.blade.php
@@ -1,5 +1,7 @@
 @extends("layouts.front")
 
+@section("title", "Welcome")
+
 @section("content")
     <div class="relative flex flex-col items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
         <div class="text-6xl font-bold my-2">Welcome to Qbixx test!</div>
