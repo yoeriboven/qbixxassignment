e0a2af2a2f44c3b378ebb058417b0649a86acd5b
Nikola Svetozarevic <nikola.svetozarevic.e@gmail.com>
2022-02-15 18:53:58 +0100

    Add example unit test

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function example test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
