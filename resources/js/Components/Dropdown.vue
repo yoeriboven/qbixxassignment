9bbe5085c0da6af31600998583b147d355a1c165
Yoeri Boven <yoeriboven@msn.com>
2022-09-01 12:33:30 +0200

    Refactored menu

---
+++ resources/js/Components/Dropdown.vue
@@ -0,0 +1,19 @@
+<template>
+<Menu as="div" class="relative inline-block text-left">
+    <div>
+      <slot name="button"></slot>
+    </div>
+
+    <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+      <MenuItems class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <div class="py-1">
+          <slot name="items"></slot>
+        </div>
+      </MenuItems>
+    </transition>
+  </Menu>
+</template>
+
+<script setup>
+import { Menu, MenuItems } from '@headlessui/vue'
+</script>
--- resources/js/Pages/Admin/Clients/Show.vue
+++ resources/js/Pages/Admin/Clients/Show.vue
@@ -4,30 +4,25 @@
     <div class="min-h-screen bg-[#575F65] font-[Montserrat]">
         <div class="bg-white px-4 py-2 flex justify-between items-center">
             <h2 class="text-"> {{ client.name }}</h2>
-            <Menu as="div" class="relative inline-block text-left">
-    <div>
-      <MenuButton class="inline-flex w-full justify-center rounded-lg bg-[#249EEA] px-4 py-2 text-[14px] font-medium text-white shadow-sm hover:bg-sky-700">
-          {{  languageString }}
-        <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
-      </MenuButton>
-    </div>
-
-    <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-      <MenuItems class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-        <div class="py-1">
-          <MenuItem v-slot="{ active }">
-            <button @click="changeLanguage('en')" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block w-full px-4 py-2 text-sm']">English</button>
-          </MenuItem>
-          <MenuItem v-slot="{ active }">
-            <button @click="changeLanguage('nl')" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block w-full px-4 py-2 text-sm']">Dutch</button>
-          </MenuItem>
-          <MenuItem v-slot="{ active }">
-            <button @click="changeLanguage('fr')" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block w-full px-4 py-2 text-sm']">French</button>
-          </MenuItem>
-        </div>
-      </MenuItems>
-    </transition>
-  </Menu>
+            <Dropdown>
+                <template #button>
+                    <MenuButton class="inline-flex w-full justify-center rounded-lg bg-[#249EEA] px-4 py-2 text-[14px] font-medium text-white shadow-sm hover:bg-sky-700">
+                        {{  languageString }}
+                        <ChevronDownIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+                    </MenuButton>
+                </template>
+                <template #items>
+                    <MenuItem v-slot="{ active }">
+                        <button @click="changeLanguage('en')" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block w-full px-4 py-2 text-sm']">English</button>
+                    </MenuItem>
+                    <MenuItem v-slot="{ active }">
+                        <button @click="changeLanguage('nl')" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block w-full px-4 py-2 text-sm']">Dutch</button>
+                    </MenuItem>
+                    <MenuItem v-slot="{ active }">
+                        <button @click="changeLanguage('fr')" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block w-full px-4 py-2 text-sm']">French</button>
+                    </MenuItem>
+                </template>
+            </Dropdown>
         </div>
         <div class="p-4">
             <div class="grid grid-cols-3 gap-4">
@@ -47,17 +42,17 @@
 </template>
 
 <script>
-import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+import { MenuButton, MenuItem } from '@headlessui/vue'
 import { ChevronDownIcon } from '@heroicons/vue/20/solid'
+import Dropdown from "@/js/Components/Dropdown";
 import { ClientItemType} from "@/js/enums.js"
 
 export default {
         components: {
-            Menu,
             MenuButton,
             MenuItem,
-            MenuItems,
-            ChevronDownIcon
+            ChevronDownIcon,
+            Dropdown
         },
 
         props: {
