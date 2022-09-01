afbeb36d2814d07185a73ace23a56e7c8cda448d
Yoeri Boven <yoeriboven@msn.com>
2022-09-01 19:05:13 +0200

    Route prefix added

--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,13 @@ use Interfaces\Front\Landing\WelcomeController;
 
 Route::get('/', WelcomeController::class)->name(RoutesEnum::FRONT_WELCOME);
 
-Route::get('admin/clients', IndexClients::class)->name(RoutesEnum::ADMIN_INDEX_CLIENTS);
-Route::get('admin/clients/{client}/edit', EditClient::class)->name(RoutesEnum::ADMIN_EDIT_CLIENT);
-Route::inertia('admin/clients/create', 'Admin/Clients/Create')->name(RoutesEnum::ADMIN_CREATE_CLIENT);
-Route::get('admin/clients/{client}', ShowClient::class)->name(RoutesEnum::ADMIN_SHOW_CLIENT);
+Route::prefix('admin')->group(function() {
+    Route::get('clients', IndexClients::class)->name(RoutesEnum::ADMIN_INDEX_CLIENTS);
+    Route::inertia('clients/create', 'Admin/Clients/Create')->name(RoutesEnum::ADMIN_CREATE_CLIENT);
+    Route::post('clients/create', StoreClient::class)->name(RoutesEnum::ADMIN_STORE_CLIENT);
+    Route::get('clients/{client}', ShowClient::class)->name(RoutesEnum::ADMIN_SHOW_CLIENT);
+    Route::get('clients/{client}/edit', EditClient::class)->name(RoutesEnum::ADMIN_EDIT_CLIENT);
+    Route::put('clients/{client}', UpdateClient::class)->name(RoutesEnum::ADMIN_UPDATE_CLIENT);
+    Route::delete('clients/{client}', DeleteClient::class)->name(RoutesEnum::ADMIN_DELETE_CLIENT);
+});
 
-Route::put('admin/clients/{client}', UpdateClient::class)->name(RoutesEnum::ADMIN_UPDATE_CLIENT);
-Route::post('admin/clients/create', StoreClient::class)->name(RoutesEnum::ADMIN_STORE_CLIENT);
-Route::delete('admin/clients/{client}', DeleteClient::class)->name(RoutesEnum::ADMIN_DELETE_CLIENT);
