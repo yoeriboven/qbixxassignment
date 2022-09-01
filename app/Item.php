468da808fd1a01254588da30a975fa43e290fab6
Yoeri Boven <yoeriboven@msn.com>
2022-09-01 10:19:10 +0200

    Created item model

---
+++ app/Item.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+}
